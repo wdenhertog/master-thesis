df957ad166de5ff28515282eea36d89a8cdbfea6
Wilbert den Hertog <wmdenhertog@gmail.com>
2020-12-21 19:14:18 +0100

    1st version of the 2D model
    First implementation of the 2D model. It probably still contains some errors, since the result 3D plot doesn't look much like a wave.

--- envelope_solver.py
+++ envelope_solver.py
@@ -2,29 +2,29 @@ import cmath
 import numpy as np
 
 
-def Lk(sign, k, dp):
+def L(sign, k, dr):
     """
     Calculation of L_k^{+-}
     :param sign: value -1, 0 or 1, which defines the -, 0, + symbol respectively
     :param k: the rho grid coordinate: 0<=k<=Np-1
-    :param dp: grid size in rho direction
+    :param dr: grid size in rho direction
     :return: L_k^{+-}
     """
     if k > 0:
         if sign == 1 or sign == -1:
-            return (1 + sign * 1 / (2 * k)) / dp ** 2
+            return (1 + sign * 1 / (2 * k)) / dr ** 2
         else:
-            return -2 / dp ** 2
+            return -2 / dr ** 2
     else:
         if sign == 1:
-            return 4 / dp ** 2
+            return 4 / dr ** 2
         elif sign == 0:
-            return -4 / dp ** 2
+            return -4 / dr ** 2
         else:
             return 0
 
 
-def Ck(sign, k, k0p, dt, dz, dp):
+def C(sign, k, k0p, dt, dz, dr):
     """
     Calculate Equation (8) from Benedetti - 2018
     :param sign: 1 or -1, which corresponds to + or - respectively in Equation (8)
@@ -32,36 +32,71 @@ def Ck(sign, k, k0p, dt, dz, dp):
     :param k0p: k0/kp
     :param dt: tau step
     :param dz: zeta step
-    :param dp: rho step
+    :param dr: rho step
     :return: C_k^{0,+-}
     """
-    # TODO: add 2nd time derivative
-    return Lk(-1, k, dp) / 2 + sign * 1j * k0p * 1 / dt - sign * 3 / 2 * 1 / (dt * dz)
+    if dr == 0:
+        return L(-1, k, dr) / 2 + sign * 1j * k0p * 1 / dt - sign * 3 / 2 * 1 / (dt * dz)  # 1D case
+    else:
+        return L(0, k, dr) / 2 + sign * 1j * k0p * 1 / dt - sign * 3 / 2 * 1 / (dt * dz) - 1 / dt ** 2  # 2D case
 
 
-def theta(a, j):
+def theta(a, j, k=0):
     """
     Calculate the phase of â
     :param a: vector â at a specific time t
     :param j: the zeta grid coordinate: 0<=j<nz
+    :param k: the rho grid coordinate: 0<=k<nr
     :return: phase of â in â(z, t)
     """
-    return cmath.phase(a[j])
+    if k == 0:
+        return cmath.phase(a[j][k])  # 1D case
+    else:
+        return cmath.phase(a[j][k])  # 2D case
 
 
-def D(a, j, dz):
+def D(a, j, k, dz):
     """
     Calculate D in Equation (6) from Benedetti - 2018
     :param a: vector â at a specific time t
     :param j: the zeta grid coordinate: 0<=j<nz
+    :param k: the rho grid coordinate: 0<=k<nr
     :param dz: zeta step
     :return: D_{j}^n
     """
-    return (-3 * theta(a, j) + 4 * theta(a, j + 1) - theta(a, j + 2)) / (2 * dz)
+    return (-3 * theta(a, j, k) + 4 * theta(a, j + 1, k) - theta(a, j + 2, k)) / (2 * dz)
 
 
 def chi(j, k, n):
-    return 0  # TODO: implement calculation of chi at time n and position (z,r) = (j,k).
+    return 1  # TODO: implement calculation of chi at time n and position (z,r) = (j,k).
+
+
+def TDMA(a, b, c, d):
+    """
+    TriDiagonal Matrix Algorithm: solve a linear system Ax=b, where A is a tridiagonal matrix.
+    Source: https://stackoverflow.com/questions/8733015/tridiagonal-matrix-algorithm-tdma-aka-thomas-algorithm-using-python-with-nump
+    :param a: lower diagonal of A
+    :param b: main diagonal of A
+    :param c: upper diagonal of A
+    :param d: solution vector
+    :return: solution to the linear system
+    """
+    n = len(d)
+    w = np.zeros(n - 1, complex)
+    g = np.zeros(n, complex)
+    p = np.zeros(n, complex)
+
+    w[0] = c[0] / b[0]  # MAKE SURE THAT b[0]!=0
+    g[0] = d[0] / b[0]
+
+    for i in range(1, n - 1):
+        w[i] = c[i] / (b[i] - a[i - 1] * w[i - 1])
+    for i in range(1, n):
+        g[i] = (d[i] - a[i - 1] * g[i - 1]) / (b[i] - a[i - 1] * w[i - 1])
+    p[n - 1] = g[n - 1]
+    for i in range(n - 1, 0, -1):
+        p[i - 1] = g[i - 1] - w[i - 1] * p[i]
+    return p
 
 
 def solve_1d(k0p, zmin, zmax, nz, dt, nt, a0):
@@ -75,30 +110,98 @@ def solve_1d(k0p, zmin, zmax, nz, dt, nt, a0):
     :param dt: size of tau step (nondimensionalized time, tau=k_pct)
     :param nt: number of tau steps
     :param a0: value for â(z,0), array of dimensions (nz, 1)
-    :return: a[z][t], 2D array of the value of â at every point zmin<=z<=zmax, and 0<=t<=dt*nt
+    :return: a[z], 1D array of the value of â at every point zmin<=z<=zmax, and t = dt*nt
     """
     # Reserve 2 rows for the ghost grid points at j = nz and j = nz+1.
     # a_old corresponds to a(z, t-1), a_current corresponds to a(z, t) and a_new corresponds to a(z, t+1)
-    # The change from coordinates in a_current is as follows: a[j] corresponds to a(zmin + j*dz, n).
-    a_old = np.concatenate((a0, np.zeros(2))).astype(complex)  # TODO: change a(z,-1) using Gaussian wave equation
+    a_old = np.concatenate((a0, np.zeros(2))).astype(complex)
     a_current = np.concatenate((a0, np.zeros(2))).astype(complex)
     a_new = np.zeros(nz + 2, dtype=complex)
 
     dz = (zmax - zmin) / (nz - 1)
 
-    c0p = Ck(1, 0, k0p, dt, dz, 0)  # C_0^{0, +}
-    c0m = Ck(-1, 0, k0p, dt, dz, 0)  # C_0^{0, -}
+    c0p = C(1, 0, k0p, dt, dz, 0)  # C_0^{0, +}
+    c0m = C(-1, 0, k0p, dt, dz, 0)  # C_0^{0, -}
 
     for n in range(0, nt):
         for j in range(nz, 0, -1):
-            factor_lhs = c0p + 1j / dt * D(a_current, j - 1, dz)
-            factor_rhs = -(c0m - 1j / dt * D(a_current, j - 1, dz)) \
+            factor_lhs = c0p + 1j / dt * D(a_current, j - 1, 0, dz)
+            factor_rhs = -(c0m - 1j / dt * D(a_current, j - 1, 0, dz)) \
                          * a_old[j - 1] \
                          - 2 * np.exp(1j * (theta(a_current, j - 1) - theta(a_current, j))) / (dt * dz) \
                          * (a_new[j] - a_old[j]) \
-                         + np.exp(1j * (theta(a_current, j - 1) - theta(a_current, j + 1))) / (dt * dz) \
+                         + np.exp(1j * (theta(a_current, j - 1) - theta(a_current, j + 1))) / (2 * dt * dz) \
                          * (a_new[j + 1] - a_old[j + 1])
             a_new[j - 1] = factor_rhs / factor_lhs
+        a_old = a_current
+        a_current = a_new
+    return a_new
+
+
+def solve_2d(k0p, zmin, zmax, nz, dt, nt, rmax, nr, a0):
+    """
+    Solve the 2D envelope equation (\nabla_tr^2+2i*k0/kp*d/dt+2*d^2/(dzdt)-d^2/dt^2)â = \chi*â
+    :param k0p: k0/kp = central laser wavenumber (2pi/lambda_0) / plasma skin depth
+    :param zmin: minimum value for zeta (nondimensionalized z-direction, zeta=k_p(z-ct))
+    :param zmax: maximum value for zeta
+    :param nz: number of grid points in zeta-direction
+    :param dt: size of tau step (nondimensionalized time, tau=k_pct)
+    :param nt: number of tau steps
+    :param rmax: maximum value for rho (minimum value is always 0). rho = k_p*r (nondimensionalized radius)
+    :param nr: number of rho steps
+    :param a0: value for â(z,r,0), array of dimensions (nz, nr)
+    :return: a[z][r], 2D array of the value of â at every point zmin<=z<=zmax, 0<=r<rmax and t = dt*nt
+    """
+    # a_old corresponds to a(z, r, t-1), a_current corresponds to a(z, r, t) and a_new corresponds to a(z, r, t+1)
+    a_old = np.r_[a0, np.zeros((2, nr))].astype(complex)  # TODO: implement function for a(z,r,-1)
+    a_current = np.r_[a0, np.zeros((2, nr))].astype(complex)  # add 2 rows of ghost points in the zeta direction
+    a_new = np.zeros((nz + 2, nr), dtype=complex)
+
+    dz = (zmax - zmin) / (nz - 1)
+    dr = rmax / (nr - 1)
+    for t in range(0, nt):
+        print("Time =", t)
+        # For every j, solve the tridiagonal system to calculate the solution over on the radius
+        for j in range(nz, 0, -1):
+            # boundary conditions for k = 0 and k = nr - 1
+            d_upper = d_lower = np.zeros(nr - 1).astype(complex)
+            d_main = sol = np.zeros(nr).astype(complex)
+            d_main[0] = C(1, 0, k0p, dt, dz, dr) - chi(j - 1, 0, t) / 2 + 1j / dt * D(a_current, j - 1, 0, dz)
+            d_main[-1] = (C(1, nr - 1, k0p, dt, dz, dr)
+                          - chi(j - 1, nr - 1, t) / 2 + 1j / dt * D(a_current, j - 1, nr - 1, dz))
+            sol[0] = (-2 / dt ** 2 * a_current[j - 1][0]
+                      - (C(-1, 0, k0p, dt, dz, dr) - chi(j - 1, 0, t) - 1j / dt * D(a_current, j - 1, 0, dz))
+                      * a_old[j - 1][0]
+                      - L(1, 0, dr) / 2 * a_old[j - 1][1]
+                      - 2 * np.exp(1j * (theta(a_current, j - 1, 0) - theta(a_current, j, 0))) / (dt * dz)
+                      * (a_new[j][0] - a_old[j][0])
+                      + np.exp(1j * (theta(a_current, j - 1, 0) - theta(a_current, j + 1, 0))) / (2 * dt * dz)
+                      * (a_new[j + 1][0] - a_old[j + 1][0]))
+            m = nr - 1  # using m instead of nr - 1 so the code occupies less width
+            sol[-1] = (-2 / dt ** 2 * a_current[j - 1][m] - L(-1, m, dr) / 2 * a_old[j - 1][m - 1]
+                       - (C(-1, m, k0p, dt, dz, dr) - chi(j - 1, m, t) - 1j / dt * D(a_current, j - 1, m, dz))
+                       * a_old[j - 1][m]
+                       - 2 * np.exp(1j * (theta(a_current, j - 1, m) - theta(a_current, j, m))) / (dt * dz)
+                       * (a_new[j][m] - a_old[j][m])
+                       + np.exp(1j * (theta(a_current, j - 1, m) - theta(a_current, j + 1, m))) / (2 * dt * dz)
+                       * (a_new[j + 1][m] - a_old[j + 1][m]))
+
+            for k in range(1, nr - 1):
+                d_main[k] = C(1, k, k0p, dt, dz, dr) - chi(j - 1, k, t) / 2 + 1j / dt * D(a_current, j - 1, k, dz)
+                sol[k] = (-2 / dt ** 2 * a_current[j - 1][k]
+                          - L(-1, k, dr) / 2 * a_old[j - 1][k - 1]
+                          - (C(-1, k, k0p, dt, dz, dr) - chi(j - 1, k, t) - 1j / dt * D(a_current, j - 1, k, dz))
+                          * a_old[j - 1][k]
+                          - L(1, k, dr) / 2 * a_old[j - 1][k + 1]
+                          - 2 * np.exp(1j * (theta(a_current, j - 1, k) - theta(a_current, j, k))) / (dt * dz)
+                          * (a_new[j][k] - a_old[j][k])
+                          + np.exp(1j * (theta(a_current, j - 1, k) - theta(a_current, j + 1, k))) / (2 * dt * dz)
+                          * (a_new[j + 1][k] - a_old[j + 1][k]))
+                d_lower[k - 1] = L(-1, k, dr) / 2
+                d_upper[k - 1] = L(1, k, dr) / 2
+            d_lower[nr - 2] = L(-1, nr - 2, dr) / 2
+            d_upper[nr - 2] = L(1, nr - 2, dr) / 2
+            a_new[j - 1] = TDMA(d_lower, d_main, d_upper, sol)
         a_old = a_current
         a_current = a_new
     return a_new
--- test.py
+++ test.py
@@ -1,19 +1,43 @@
 from envelope_solver import *
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 k0p = 100
-zmin = -10
+zmin = -5
 zmax = 5
-nz = 100
+nz = 1000
 dt = 1
-nt = 1500
-amp = 1.5
-L = 1
+nt = 10
+rmax = 5
+nr = 100
+a0 = 1
+sz = np.sqrt(2)
+sr = np.sqrt(2)
 
 Z = np.linspace(zmin, zmax, nz)
-a0 = amp * np.sqrt(np.exp(-Z ** 2 / (2 * L ** 2)))
+R = np.linspace(0, rmax, nr)
+ZZ, RR = np.meshgrid(R, Z)
+a_env0 = a0 / np.sqrt(2) * np.sqrt(np.exp(-Z ** 2 / (sz ** 2)))
+a_2d0 = np.zeros((nz, nr))
+for i in range(nz):
+    for j in range(nr):
+        a_2d0[i][j] = a0 / np.sqrt(2) * np.sqrt(np.exp(-Z[i] ** 2 / (sz ** 2) - R[j] ** 2 / (sr ** 2)))
 
-a = solve_1d(k0p, zmin, zmax, nz, dt, nt, a0)
+# a = solve_1d(k0p, zmin, zmax, nz, dt, nt, a_env0)
+# a2d3 = solve_2d(k0p, zmin, zmax, nz, dt, 3, rmax, nr, a_2d0)
+# a2d5 = solve_2d(k0p, zmin, zmax, nz, dt, 5, rmax, nr, a_2d0)
+a2d = solve_2d(k0p, zmin, zmax, nz, dt, nt, rmax, nr, a_2d0)
 
-plt.plot(Z, abs(a[0:-2]))
+# plt.plot(Z, a_env0)
+# plt.plot(Z, abs(a2d[0:-2, 0]))
+# plt.plot(Z, abs(a_2d0[:, 9]))
+# plt.show()
+fig1 = plt.figure()
+ha = fig1.add_subplot(111, projection='3d')
+ha.plot_surface(ZZ, RR, a_2d0)
+
+fig2 = plt.figure()
+hb = fig2.add_subplot(111, projection='3d')
+hb.plot_surface(ZZ, RR, abs(a2d[0:-2, :]))
+
 plt.show()
